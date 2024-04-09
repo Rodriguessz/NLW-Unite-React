e6b581ddd804e5428a1281d95f7de6bd6ced262d
Enzo Rodrigues <enzo.orodrigues03@gmail.com>
2024-04-09 18:40:03 -0300

    Table list

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -2,7 +2,7 @@
 export function AttendeeList() {
   return (
     <>
-      <div>
+      <div className="flex flex-col gap-4">
 
         {/* Header Table */}
         <div className="flex gap-5 items-center">
@@ -17,35 +17,41 @@ export function AttendeeList() {
           <table className="w-full">
             <thead>
               <tr className="border-b border-white/10">
-                <th className="py-3 px-2.5 text-sm text-semibold"> <input type="checkbox" /> </th>
-                <th className="py-3 px-2.5 text-sm text-semibold">Código</th>
-                <th className="py-3 px-2.5 text-sm text-semibold">Participante</th>
-                <th className="py-3 px-2.5 text-sm text-semibold">Data de Inscrição</th>
-                <th className="py-3 px-2.5 text-sm text-semibold">Data do Check-in</th>
-                <th className="py-3 px-2.5 text-sm text-semibold"></th>
+                <th className="py-3 px-4 text-sm text-semibold text-left"> <input type="checkbox" /> </th>
+                <th className="py-3 px-4 text-sm text-semibold text-left">Código</th>
+                <th className="py-3 px-4 text-sm text-semibold text-left">Participante</th>
+                <th className="py-3 px-4 text-sm text-semibold text-left">Data de Inscrição</th>
+                <th className="py-3 px-4 text-sm text-semibold text-left">Data do Check-in</th>
+                <th className="py-3 px-4 text-sm text-semibold text-left"></th>
               </tr>
             </thead>
 
             <tbody>
-              <tr>
-                <td> <input type="checkbox" /> </td>
-                <td> 192945</td>
-                <td>
-                  <div className="flex flex-col items-start">
-                    <span>Enzo Rodrigues</span>
-                    <span>enzo.orodrigues03@gmail.com</span>
-                  </div>
-                </td>
-                <td> 7 dias atrás </td>
-                <td> 7 dias atrás </td>
-                <td></td>
-              </tr>
+              {/* Exibindo itens repetidos sem precisar copiar várias vezes */}
+
+              {Array.from({ length: 5 }).map(() => {
+                return (
+                  <tr className="border-b border-white/10">
+                    <td className="py-3 px-4 text-sm text-zinc-300"> <input type="checkbox" /> </td>
+                    <td className="py-3 px-4 text-sm text-zinc-300"> 192945</td>
+                    <td className="py-3 px-4 text-sm text-zinc-300">
+                      <div className="flex flex-col gap-1">
+                        <span className="font-semibold text-white">Enzo Rodrigues</span>
+                        <span>enzo.orodrigues03@gmail.com</span>
+                      </div>
+                    </td>
+                    <td className="py-3 px-4 text-sm text-zinc-300"> 7 dias atrás </td>
+                    <td className="py-3 px-4 text-sm text-zinc-300"> 7 dias atrás </td>
+                    <td className="py-3 px-4 text-sm text-zinc-300"></td>
+                  </tr>
+                )
+              })}
             </tbody>
 
             <tfoot>
               <tr>
-                <td colSpan={3}>Mostrando 10 de 228 itens</td>
-                <td colSpan={3}>Página 1 de 23</td>
+                <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>Mostrando 10 de 228 itens</td>
+                <td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>Página 1 de 23</td>
               </tr>
             </tfoot>
           </table>
